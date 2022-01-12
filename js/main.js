67ee77582974460b795b0b886ccf64eaf8b9e6e8
leopoldo <villaleoo@hotmail.com>
2022-01-12 19:03:55 -0300

    creando los tres algoritmos que marcan como ejemplo

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*let datito = parseInt(prompt("ingrese un numero"))
+for (datito;datito<=10;datito++){
+    let result = datito + 3;
+    alert("el resultado de sumarle 3 al numero que ingresaste es:" + result) //"datito" comienza con el numero que ingresas y le suma 3,al aceptar "datito" se incrementa en 1 y a ese nuevo valor de "datito" se le suma 3, "datito" puede valer HASTA 10
+}*/
+
+/*let texto = prompt("ingrese una palabra")
+for(i=1; texto!="esc";i++){
+    alert("la palabra que ingresaste es:" +i+" "+ texto)
+    if (i===5){
+        break
+    }
+}*/
+
+alert("bienvenido")
+let dato = parseInt(prompt("ingrese un numero"))
+for(num=1;num<=dato;num++){
+    alert("esto se repite el numero de veces correspondiente al numero que ingresaste :O")
+}
+
+
+
+
